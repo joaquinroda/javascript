d41588f66702b345ebea30f308561473f893c86b
joaquin roda <joaquin-roda@hotmail.com>
2022-07-18 21:39:00 -0300

    Agregue ejemplos para el entregable

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// Práctica js
+
+
+// Practica IF
+
+// let precio = prompt('Ingrese el precio del juego en steam')
+
+// if (precio > 0) {
+
+//     let precioFinal = precio*1.75
+
+//     console.log('El precio del juego en steam con impuestos incluídos es ' + precioFinal);
+// } else {
+//     console.log('Ese no es un precio válido');
+// }
+
+
+// Practica For/while
+
+
+console.log('Tabla del 3')
+
+for (let t3 = 0; t3 <= 300; t3 = t3 + 3) {
+
+    if(t3 !=300) {
+    console.log(t3)
+    }
+}
+
+console.log('Fin');
